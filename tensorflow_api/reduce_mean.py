802e115ef97fd1d702b8170440d4e7421c5e8cb7
learning17 <841221809@qq.com>
2017-12-28 22:21:23 +0800

    ci

--- tensorflow_api/reduce_mean.py
+++ tensorflow_api/reduce_mean.py
@@ -18,9 +18,9 @@ x = tf.Variable(initial,dtype=tf.float32)
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init_op)
-    print sess.run(tf.reduce_mean(x))
-    print sess.run(tf.reduce_mean(x,0,keep_dims=True)) #Column
-    print sess.run(tf.reduce_mean(x,1,keep_dims=True)) #row
+    print(sess.run(tf.reduce_mean(x)))
+    print(sess.run(tf.reduce_mean(x,0,keep_dims=True))) #Column
+    print(sess.run(tf.reduce_mean(x,1,keep_dims=True))) #row
 
 
 
