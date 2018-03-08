717d0506081c79445f4dcf120955be20cc0e2eb8
learning17 <841221809@qq.com>
2018-03-08 20:41:02 +0800

    data

--- tensorflow_api/Dataset.py
+++ tensorflow_api/Dataset.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
 
 def parse(img, label):
-    return "1",2
+    return img,label
 
 train_imgs = tf.constant(['train/img1.png', 'train/img2.png',
                             'train/img3.png', 'train/img4.png',
                             'train/img5.png', 'train/img6.png'])
 train_labels = tf.constant([0, 0, 0, 1, 1, 1])
 data = tf.data.Dataset.from_tensor_slices((train_imgs, train_labels))
-
 data1 = data.map(parse)
 data = data.concatenate(data1)
 
